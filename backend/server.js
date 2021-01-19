b6e43791731853059f6235d451e7bd929219ced0
Ris3r <amitlekhak@yahoo.com>
2021-01-19 20:26:30 +0545

    Initial setup with express,mongoose and dotnet config

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//Setting up config file
+dotenv.config({ path: "backend/config/config.env" });
+
+//Connecting to database
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `App running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
