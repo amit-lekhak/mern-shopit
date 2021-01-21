01e80087bd277d596833b4be7f416cdbf612ff1f
Ris3r <amitlekhak@yahoo.com>
2021-01-21 18:47:28 +0545

    Add errror handlers for async,prromise rejection,uncaught exception and mongoose validator

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,31 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+
+});
+
 //Setting up config file
 dotenv.config({ path: "backend/config/config.env" });
 
 //Connecting to database
-
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `App running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
